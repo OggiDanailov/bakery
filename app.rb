b7b01a6b46bd94cb96cbb438801288ef248c7f07
Oggi Danailov <ogidan@abv.bg>
2018-10-26 23:05:20 -0400

    signin added

--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
 require 'sinatra'  
-require 'sinatra/activerecord'  
+require 'sinatra/activerecord' 
+require 'sinatra/flash' 
 set :database, 'sqlite3:bakery.sqlite3'  
 require './models'
 
+set :sessions, true
 
-get "/" do 	
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+end
+
+get "/" do 
+
 	erb :home
 end
 
@@ -14,10 +22,30 @@ post "/signup" do
 	user = User.new(username: username, password: password)
 	if user.save
 		redirect "/products"
+		
 	else 
+
 		erb :home
 	end  
 end
+
+post "/login" do 
+	if user = User.where(username: params[:username]).first
+		if user.password == params[:password]
+			session[:user_id] = user.id 
+			flash[:notice] = "Successfully signed up"
+			redirect "/products"
+		else 
+			flash[:notice] = "try again"
+			redirect "/"
+		end
+	else 
+		flash[:notice] = "try again"
+		redirect "/"
+	end
+end
+
+
 
 get "/products" do
 
